efe67563143fb673614ffa16872e234deedb3a96
KarolyJ <jonaskaroly15@gmail.com>
2024-01-04 19:23:14 +0100

    Some style changes
    Realized a bug when deleting items, on the app the input text doesn't show on the app

--- App.js
+++ App.js
@@ -1,46 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
-import { useState } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
-import ListItem from './components/ListItem';
+import ListItem from "./components/ListItem";
 
 const today = new Date();
 let nextId = 0;
 
 function formatDate(date) {
-  return new Intl.DateTimeFormat(
-    'en-US',
-    { weekday: 'long' }
-  ).format(date);
+  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
 }
 
-
 export default function App() {
-  const[ToDoName, setToDoName] = useState('');
+  const [ToDoName, setToDoName] = useState("");
   const [ToDoList, setToDoList] = useState([]);
 
   const onAddBtnClick = (Text) => {
-    const newItem = <ListItem key={ToDoList.length} ListText={Text} delFromList={delFromlist}/>;
-    setToDoList([...ToDoList, newItem]);
-  }
+    if (Text !== "") {
+      const newItem = (
+        <ListItem
+          key={ToDoList.length}
+          ListText={Text}
+          delFromList={delFromList}
+        />
+      );
+      setToDoList([...ToDoList, newItem]);
+    } else console.log("write something");
+  };
 
-  const delFromlist = () => {
-    setToDoList(ToDoList.filter(a => a.key !== ToDoList.key))
-  }
+  const delFromList = () => {
+    setToDoList(ToDoList.filter((a) => a.key !== ToDoList.key));
+  };
 
   return (
     <View style={styles.container}>
-      <h1>To Do List for {formatDate(today)}</h1>
-      <View style={styles.mainContainer}>
-        {ToDoList}
+      <Text style={styles.h1}>To Do List for {formatDate(today)}</Text>
+      <View style={styles.mainContainer}>{ToDoList}</View>
+      <View style={styles.buttomContainer}>
+        <Button title="+" onPress={() => onAddBtnClick(ToDoName)} />
+        <TextInput
+          placeholder="Write here"
+          onChange={(e) => setToDoName(e.target.value)}
+          style={{ height: 28 }}
+        />
       </View>
-        <View style={styles.buttomContainer}>
-          <Button title='+' onPress={() => onAddBtnClick(ToDoName)}/>
-          <TextInput
-            placeholder="useless placeholder"
-            onChange={e => setToDoName(e.target.value)}
-          />
-        </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -49,15 +52,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f5f4e6',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f5f4e6",
+    alignItems: "center",
+    justifyContent: "center",
   },
   mainContainer: {
-    backgroundColor: 'white',
+    backgroundColor: "",
   },
   buttomContainer: {
-    alignItems: 'center',
-    flexDirection: 'row',
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
--- components/ListItem.js
+++ components/ListItem.js
@@ -2,23 +2,30 @@ import { View, StyleSheet } from "react-native";
 import BouncyCheckbox from "react-native-bouncy-checkbox";
 import Icon from "react-native-vector-icons/FontAwesome";
 
-export default function ListItem({ListText, delFromList}) {
-    return(
-        <View>  
-            <BouncyCheckbox
-                size={25}
-                fillColor="red"
-                unfillColor="#FFFFFF"
-                text= {ListText}
-                iconStyle={{ borderColor: "red" }}
-                />
-            <Icon name="close" size={20} onPress={delFromList}/>
-        </View>
-    )
+export default function ListItem({ ListText, delFromList }) {
+  return (
+    <View style={styles.ItemContainer}>
+      <BouncyCheckbox
+        size={25}
+        fillColor="red"
+        unfillColor="#FFFFFF"
+        text={ListText}
+        iconStyle={{ borderColor: "red" }}
+      />
+      <Icon
+        name="close"
+        size={20}
+        onPress={delFromList}
+        style={{ marginLeft: 20 }}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    ItemContainer: {
-        flexDirection: 'row',
-    },
+  ItemContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    marginVertical: 5,
+  },
 });
