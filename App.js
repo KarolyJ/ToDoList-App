6a504b5e9df561a0e007b7eaeda358fd3f73be23
KarolyJ <jonaskaroly15@gmail.com>
2024-02-14 11:59:46 +0100

    Added TODOs

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={DoListPage} />
       </Stack.Navigator>
     </NavigationContainer>
