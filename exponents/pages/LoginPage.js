1d54c16a5d33199b637c9df3bb74e51e107e92be
KarolyJ <jonaskaroly15@gmail.com>
2024-02-14 11:36:39 +0100

    Starting Naigation

---
+++ exponents/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+export default function LoginPage() {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
