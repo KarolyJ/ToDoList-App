69b42285afe309a56563c11f30f159b747f4d561
KarolyJ <jonaskaroly15@gmail.com>
2024-02-14 11:47:07 +0100

    Pages are managed with Navigationn

--- App.js
+++ App.js
@@ -1,91 +1,19 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  StyleSheet,
-  Text,
-  TextInput,
-  View,
-  FlatList,
-  TouchableOpacity,
-} from "react-native";
-import { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import DoListPage from "./exponents/pages/DoListPage";
+import LoginPage from "./exponents/pages/LoginPage";
 
-const today = new Date();
-
-function formatDate(date) {
-  return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
-}
+/* createNativeStackNavigator is a function that returns an object containing 2 properties:
+Screen and Navigator. Both of them are React components used for configuring the navigator.
+The Navigator should contain Screen elements as its children to define the configuration for routes. */
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const [Task, setTask] = useState("");
-  const [ToDoList, setToDoList] = useState([]);
-
-  const handleAddTask = () => {
-    if (Task.trim() !== "") {
-      //create new task with unique ID
-      const newTask = { id: Math.random(), text: Task };
-
-      //update the task array
-      setToDoList([...ToDoList, newTask]);
-      //clear input field
-      setTask("");
-    }
-  };
-
-  const renderItem = ({ item }) => (
-    <View>
-      <Text>{item.text}</Text>
-      <TouchableOpacity onPress={() => handleDeleteFromList(item.id)}>
-        <Text style={styles.deleteButton}>Delete</Text>
-      </TouchableOpacity>
-    </View>
-  );
-
-  const handleDeleteFromList = (itemId) => {
-    const updatedList = ToDoList.filter((ToDoName) => ToDoName.id !== itemId);
-    setToDoList(updatedList);
-  };
-
   return (
-    <View style={styles.container}>
-      <Text style={styles.h1}>To Do List for {formatDate(today)}</Text>
-      <View>
-        <FlatList
-          data={ToDoList}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleAddTask}>
-          <Text>Add Task</Text>
-        </TouchableOpacity>
-        <TextInput
-          placeholder="Write here"
-          value={Task}
-          onChangeText={(text) => setTask(text)}
-          style={{ height: 28 }}
-        />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={DoListPage} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "green",
-    justifyContent: "center",
-    padding: 20,
-  },
-  mainContainer: {
-    backgroundColor: "",
-  },
-  buttonContainer: {
-    alignItems: "center",
-    flexDirection: "row",
-  },
-  deleteButton: {
-    color: "red",
-  },
-});
---
+++ exponents/pages/DoListPage.js
@@ -0,0 +1,94 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+
+// function formatDate(date) {
+//     return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+//   }
+
+export default function DoListPage() {
+  const today = new Date();
+  const [Task, setTask] = useState("");
+  const [ToDoList, setToDoList] = useState([]);
+
+  const formateDate = ({ date }) => {
+    return new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(date);
+  };
+
+  const handleAddTask = () => {
+    if (Task.trim() !== "") {
+      //create new task with unique ID
+      const newTask = { id: Math.random(), text: Task };
+
+      //update the task array
+      setToDoList([...ToDoList, newTask]);
+      //clear input field
+      setTask("");
+    }
+  };
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Text>{item.text}</Text>
+      <TouchableOpacity onPress={() => handleDeleteFromList(item.id)}>
+        <Text style={styles.deleteButton}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  const handleDeleteFromList = (itemId) => {
+    const updatedList = ToDoList.filter((ToDoName) => ToDoName.id !== itemId);
+    setToDoList(updatedList);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.h1}>To Do List for {formateDate(today)}</Text>
+      <View>
+        <FlatList
+          data={ToDoList}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </View>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleAddTask}>
+          <Text>Add Task</Text>
+        </TouchableOpacity>
+        <TextInput
+          placeholder="Write here"
+          value={Task}
+          onChangeText={(text) => setTask(text)}
+          style={{ height: 28 }}
+        />
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "green",
+    justifyContent: "center",
+    padding: 20,
+  },
+  mainContainer: {
+    backgroundColor: "",
+  },
+  buttonContainer: {
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  deleteButton: {
+    color: "red",
+  },
+});
